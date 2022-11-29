dd45ed86ea0e3931a3ff2879fd1aa2351e3b5c70
Peter Maltby <peter.p.maltby@gmail.com>
2022-11-29 17:48:30 +0000

    added fullscreen patch.

--- config.def.h
+++ config.def.h
@@ -61,6 +61,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	             	XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,             		XK_q,      killclient,     {0} },
+	{ MODKEY,             		XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
